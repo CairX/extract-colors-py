cde1d94bc10fa39619196a1cff2a5e29a95de5da
cairns <lazycairx@gmail.com>
2017-07-09 14:54:06 +0200

    Store result to image for a visual representation

--- extractor.py
+++ extractor.py
@@ -1,27 +1,69 @@
-from PIL import Image
+import argparse
+import os
+import time
+from collections import defaultdict
+from PIL import Image, ImageDraw
 
-im = Image.open('gameboy.png', 'r')
-width, height = im.size
-print(im.mode)
-pixel_values = list(im.getdata())
+
+def load(path):
+	im = Image.open(path)
+	pixels = list(im.getdata())
+	width, height = im.size
+
+	print("\n[IMAGE]")
+	print("Width: {}".format(width))
+	print("Height: {}".format(height))
+	print("Pixels: {}".format(len(pixels)))
+
+	return pixels
 
 
-pixels = len(pixel_values)
-print("\n[ RESULT {}]".format(pixels))
-print(width)
-print(height)
+def count_colors(pixels):
+	counter = defaultdict(int)
+	for color in pixels:
+		counter[color] += 1
 
-counter = dict()
-for i in range(pixels):
-    color = pixel_values[i]
+	print("\n[COUNTER]")
+	print("Colors: {}".format(len(counter)))
 
-    # print(i)
-    # print(color)
+	return sorted(counter.items(), key=lambda x: x[1], reverse=True)
 
-    if color in counter:
-        counter[color] += 1
-    else:
-        counter[color] = 1
 
-for key, value in sorted(counter.items(), key=lambda x: x[1], reverse=True):
-    print("{0:15}:{1:>7}% ({2})".format(str(key), "{0:.2f}".format(value / pixels * 100), value))
+def print_result(counter, total):
+	print("\n[RESULT]")
+	for key, value in counter:
+		print("{0:15}:{1:>7}% ({2})".format(str(key), "{0:.2f}".format(value / total * 100), value))
+
+
+def image_result(counter, size, path):
+	result = Image.new("RGBA", (len(counter) * size, size), (0, 0, 0, 0))
+	canvas = ImageDraw.Draw(result)
+	for idx, item in enumerate(counter):
+		canvas.rectangle([(idx * size, 0), ((idx * size) + (size - 1), (size - 1))], fill=item[0])
+
+	file_name = os.path.splitext(os.path.basename(path))[0]
+	file_name = "{0} {1}.png".format(file_name, time.strftime("%Y-%m-%d %H%M%S", time.gmtime()))
+	result.save(os.path.join("results", file_name), "PNG")
+
+
+def main():
+	parser = argparse.ArgumentParser(
+		description="Extract the most common colors from an image.")
+	parser.add_argument(
+		"image",
+		nargs=1,
+		metavar="PATH")
+	args = parser.parse_args()
+
+	path = args.image[0]
+	pixels = load(path)
+	counter = count_colors(pixels)
+	wanted = min(10, len(counter))
+	counter = counter[:wanted]
+
+	print_result(counter, len(pixels))
+	image_result(counter, 150, path)
+
+
+if __name__ == "__main__":
+	main()
