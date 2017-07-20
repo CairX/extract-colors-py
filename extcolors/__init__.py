e9034f7cc1cb6f04ac30e09d6ab00176a33c138c
Thomas Cairns <lazycairx@gmail.com>
2017-07-20 16:42:25 +0200

    Transform the project into a python package

--- extcolors/__init__.py
+++ extcolors/__init__.py
@@ -1,5 +1,5 @@
 import argparse
-import colorutil
+import extcolors.colorutil
 import math
 import os
 import time
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+	name="extcolors",
+	version="0.0.1",
+	description="Extract colors from a specified image. "
+				"Groups colors based on visual similarities using the CIE76 formula.",
+	url="", # INSERT GITHUB HERE
+	author="CairX",
+	author_email="lazycairx@gmail.com",
+	license="MIT",
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Natural Language :: English",
+		"Environment :: Console",
+
+		"Topic :: Multimedia :: Graphics",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Utilities",
+
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.4"
+	],
+	keywords="extract colors image",
+	packages=["extcolors"],
+	install_require=["Pillow"],
+	entry_points={
+		"console_scripts": [
+			"extcolors=extcolors:main"
+		],
+	},
+)
