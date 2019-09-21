8195f42c11ba9adba3e175588a9df41b7f877f78
Thomas Cairns <lazycairx@gmail.com>
2019-09-21 10:25:45 +0200

    Set description type for package

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
 	description="Extract colors from an image. "
 				"Colors are grouped based on visual similarities using the CIE76 formula.",
 	long_description=open("README.rst").read(),
+	long_description_content_type="text/markdown",
 	url="https://github.com/CairX/extract-colors-py",
 	author="CairX",
 	author_email="lazycairx@gmail.com",
