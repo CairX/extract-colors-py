379d8234544132e210a6788e40fc26658cfc985d
Thomas Cairns <thomas@cairns.se>
2020-06-29 10:16:50 +0200

    Drop support for Python 3.4
    PEP 429 announced that end-of-life was reached for the version on
    March the 18th 2019.
    
    https://www.python.org/dev/peps/pep-0429/

--- setup.py
+++ setup.py
@@ -22,10 +22,14 @@ setup(
 		"Topic :: Software Development :: Libraries :: Python Modules",
 		"Topic :: Utilities",
 
+		"Programming Language :: Python :: 2.7",
 		"Programming Language :: Python :: 3",
-		"Programming Language :: Python :: 3.4"
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
 	],
-	python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+	python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
 	keywords="extract colors image",
 	packages=["extcolors"],
 	install_requires=["Pillow"],
